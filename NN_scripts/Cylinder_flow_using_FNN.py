1160c0482549b9339dcb16918f3b513d23912d54
ruchitkini <ruchitkini@gmail.com>
2024-09-23 10:00:30 +0200

    Multiple hidden layers added, update on normalising data

--- NN_scripts/Cylinder_flow_using_FNN.py
+++ NN_scripts/Cylinder_flow_using_FNN.py
@@ -28,7 +28,7 @@ input_data = []
 output_data = []
 
 # Extract the data from each file
-for file in csv_files:
+for i, file in enumerate(csv_files):
     df = pd.read_csv(file, skiprows=5)
 
     features = df[['X [ m ]', ' Y [ m ]']].values  # Extracting x and y coordinates
@@ -43,8 +43,10 @@ output_data = np.array(output_data).reshape(-1, 3)
 
 scaler = MinMaxScaler()
 scaler.fit(output_data)
-input_data = scaler.fit_transform(input_data)  # Normalize input features
-output_data = scaler.fit_transform(output_data) # Normalize output data
+# Save the scaler during training
+torch.save(scaler, 'scaler.pth')
+# input_data = scaler.fit_transform(input_data)  # Normalize input features
+output_data = scaler.fit_transform(output_data)  # Normalize output data
 
 # Now convert numpy arrays to PyTorch tensors and move them to the device (GPU or CPU)
 input_data = torch.tensor(input_data).float().to(device)
@@ -53,9 +55,8 @@ output_data = torch.tensor(output_data).float().to(device)
 # Split into training and testing sets
 train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(input_data, output_data, test_size=0.2,
                                                                           random_state=42)
-
 # Create PyTorch DataLoader for batch processing
-batch_size = 32
+batch_size = 512
 
 # Convert train and test data to TensorDataset
 train_dataset = TensorDataset(train_inputs, train_outputs)
@@ -66,38 +67,42 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
 class FNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
+    def __init__(self, input_dim, hidden_dims, output_dim):
         super(FNN, self).__init__()
         # Linear function, first hidden layer
-        self.fc1 = nn.Linear(input_dim, hidden_dim1)
-        # Second hidden layer
-        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2)
-        # Activation function
-        self.tanh = nn.Tanh()
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        # Loop of hidden layers
+        self.hidden_layers = nn.ModuleList()
+        for i in range(1, len(hidden_dims)):
+            self.hidden_layers.append(nn.Linear(hidden_dims[i-1], hidden_dims[i]))
         # Linear function (readout)
-        self.fc3 = nn.Linear(hidden_dim2, output_dim)
+        self.output_layer = nn.Linear(hidden_dims[-1], output_dim)
+        # Activation function
+        self.activation = nn.Tanh()
 
     def forward(self, x):
-        out = self.fc1(x)  # Linear transform (input)
-        out = self.tanh(out)  # Non-linearity
-        # Forward pass through the second hidden layer
-        out = self.fc2(out)
-        out = self.tanh(out)
-
-        out = self.fc3(out)  # Linear transform (output)
+        out = self.input_layer(x)  # Input layer
+        out = self.activation(out)  # Non-linearity
+        # Forward pass through all the hidden layer
+        for layer in self.hidden_layers:
+            out = layer(out)
+            out = self.activation(out)
+        # Output layer
+        out = self.output_layer(out)
 
         return out
 
 
 # Create the model and move it to the device (GPU or CPU)
-model = FNN(input_dim=2, hidden_dim1=30, hidden_dim2=30, output_dim=3).to(device)
+hidden_layers = [64] * 4  # neurons * no. of hidden layers
+model = FNN(input_dim=2, hidden_dims=hidden_layers, output_dim=3).to(device)
 loss_function = nn.MSELoss()
 lr = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr)
 
 # Training loop
 iter = 0
-num_epochs = 200
+num_epochs = 500
 print("Training the model ...")
 start_time = time.time()
 best_loss = float('inf')
@@ -113,12 +118,12 @@ for epoch in range(num_epochs):
         loss.backward()  # Backward pass: Compute gradients
         optimizer.step()  # Update model parameters
 
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():2.2e}')
 
     interval = 1
     # Save the model after a specific epoch
     if (epoch + 1) % interval == 0 and loss < best_loss:
-        best_loss = loss
+        best_loss = loss.item()
         model_path = f'Cylinder_flow_model/model_epoch_{epoch + 1}.pth'
         torch.save(model.state_dict(), model_path)
         print(f'Model saved at epoch {epoch + 1} to {model_path}')
